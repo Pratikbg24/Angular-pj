dabd970d6b9f8abad88baac3da7511d1f4c2365c
Pratikbg24 <pratikghongade24@gmail.com>
2020-03-01 18:18:58 +0530

    New Changes

--- MyApp/src/app/app-routing.module.ts
+++ MyApp/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{Home1Component} from '../app/component/home1/home1.component';
+import{LoginComponent} from '../app/component/login/login.component'
+import { from } from 'rxjs';
+
+const routes: Routes = [
 
 
-const routes: Routes = [];
+{
+  path:'home1',
+  component:Home1Component
+
+},
+{
+  path:'',
+  component:LoginComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
