97368ef3edfa0461b4e71219392d29239760941b
thisissomnath <sonawanesomanath543@gmail.com>
2020-03-02 08:57:12 +0530

    Soma Changes

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { Home1Component } from './component/home1/home1.component';
+// import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+// import { NgxSpinnerModule } from "ngx-spinner";
+  
 
 @NgModule({
   declarations: [
