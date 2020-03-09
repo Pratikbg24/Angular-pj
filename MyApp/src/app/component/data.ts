9340f6ab6922b31cce7cd54a6ab72777d78c262f
Pratikbg24 <pratikghongade24@gmail.com>
2020-03-09 14:21:15 +0530

    change

--- MyApp/src/app/component/data.ts
+++
@@ -1,5 +0,0 @@
-
-export interface Data {
-    month: String;
-    price: Number;
-  }
--- MyApp/src/app/component/home1/home1.component.ts
+++ MyApp/src/app/component/home1/home1.component.ts
@@ -4,7 +4,7 @@ import{HttpClient} from '@angular/common/http';
 import { Chart} from 'chart.js';
 import { from } from 'rxjs';
 import {observable} from 'rxjs'
-import {Data} from '../data';
+
  @Component({
   selector: 'app-home1',
   templateUrl: './home1.component.html',
@@ -12,48 +12,9 @@ import {Data} from '../data';
 })
 export class Home1Component implements OnInit {
   
-title='app';
-data: Data[];
-  url = 'http://localhost:4200/results';
-  month = [];
-  price = [];
-  chart = [];
-  constructor(private httpClient:HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-    this.httpClient.get(this.url).subscribe((res: Data[]) => {
-      res.forEach(y => {
-        this.month.push(y.month);
-        this.price.push(y.price);
-      });
-      this.chart = new Chart('canvas', {
-        type: 'line',
-        data: {
-          labels: this.month,
-          datasets: [
-            {
-              data: this.price,
-              borderColor: '#3cba9f',
-              fill: false
-            }
-          ]
-        },
-        options: {
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              display: true
-            }],
-            yAxes: [{
-              display: true
-            }],
-          }
-        }
-      });
-    });
-  }
-  
 
+}
 }
