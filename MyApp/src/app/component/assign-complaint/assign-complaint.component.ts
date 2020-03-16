1d652babfd4767c67290b4a0788831745ca7ee2d
Pratikbg24 <pratikghongade24@gmail.com>
2020-03-16 23:29:24 +0530

    fiter machinename

--- MyApp/src/app/component/assign-complaint/assign-complaint.component.ts
+++ MyApp/src/app/component/assign-complaint/assign-complaint.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input,Output,EventEmitter, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner'
-import { format } from 'url';
 import { ChartService } from '../../service/chart.service'
-import { from } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Data } from 'src/app/data';
 
 @Component({
   selector: 'app-assign-complaint',
   templateUrl: './assign-complaint.component.html',
   styleUrls: ['./assign-complaint.component.css']
 })
+/* 
+@Output() searchcriteria = new EventEmitter<String>();
+searchThis() {
+    this.searchcriteria.emit(this.searchword)
+} */
+
 export class AssignComplaintComponent implements OnInit {
 
+  pageTitle = 'Assign Complaints';
+
+  complaint: any[];
+  c_id: any[];
+  Machine_type: any = {
+    u_Machinepurchesed: null,
+
+  }
+  c_date: [];
   
-  complaint:any[];
-  c_id:any[];
+  searchText;
+
+  data=[
+
+  ]
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -26,24 +41,43 @@ export class AssignComplaintComponent implements OnInit {
 
   ngOnInit() {
 
-    this.charts.getComplaintData().subscribe((data:any)=>
-      {
+    this.charts.getComplaintData().subscribe((data: any) => {
 
-        this.complaint = data.data.filter((el:any)=>
-        {
-            return el.c_desc
-        })
+      this.complaint = data.data.filter((el: any) => {
+        return el.c_desc
+      })
+
+      this.c_id = data.data.filter((el: any) => {
+        return el.c_id
+      })
+      this.c_date = data.data.filter((el: any) => {
+        return el.c_date
+      })
+      this.Machine_type = data.data.filter((el: any) => {
+        return el.Machine_type == 1
+      })
+
+
+      console.log(data)
+
+
+      this.Machine_type.u_Machinepurchesed = this.Machine_type.u_Machinepurchesed;
+
       
-        this.c_id = data.data.filter((el:any)=>
-        {
-            return el.c_id
-        })
-      
-        console.log(data)
-      }
-  
-    
-    )
 
     }
+
+
+    )/* 
+    this.charts.getComplaintData().subscribe((data: any) => {
+
+      if (data) {
+        this.data= this.data.filter(function (ele, i, array) {
+          let arrayelement = ele.c_desc.toLowerCase()
+          return arrayelement.includes(data)
+        })
+      }
+
+    }) */
   }
+}
