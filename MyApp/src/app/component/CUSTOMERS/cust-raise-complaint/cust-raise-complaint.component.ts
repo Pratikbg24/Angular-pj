9aad31db7d9dc71c656e4e0a4e09bc694be99be4
Neha <kashyapneha84@gmail.com>
2020-05-15 09:49:45 +0530

    Updated the request count in cust portal

--- MyApp/src/app/component/CUSTOMERS/cust-raise-complaint/cust-raise-complaint.component.ts
+++ MyApp/src/app/component/CUSTOMERS/cust-raise-complaint/cust-raise-complaint.component.ts
@@ -22,26 +22,26 @@ export class CustRaiseComplaintComponent implements OnInit {
   maxDate: Date;
   showSuccessMsg: boolean = false;
   showInvalidMsg: boolean = false;
-  statusOpen:number[];
-  statusClosed:number[];
-  statusPending:number[];
-  countData:any={
-    openCount:0,
-    closedCount:0,
-    pendingCount:0  
+  statusOpen: number[];
+  statusClosed: number[];
+  statusPending: number[];
+  countData: any = {
+    openCount: 0,
+    closedCount: 0,
+    pendingCount: 0
   };
-  openCount1:number[];
-  closedCount1:number[];
-  pendingCount1:number[];
+  openCount1: number[];
+  closedCount1: number[];
+  pendingCount1: number[];
   complaint: any[];
- 
+
   validation_messages = {
 
     'name': [
       { type: 'required', message: '*Name is required' },
       { type: 'minlength', message: '*Name must be 3 character' }
     ],
-   'EngineerType': [
+    'EngineerType': [
       { type: 'required', message: '*Please select engineer type' },
     ],
     'DateOfjoining': [
@@ -60,9 +60,9 @@ export class CustRaiseComplaintComponent implements OnInit {
     this.maxDate = new Date();
     this.maxDate.setDate(this.maxDate.getDate() + 0);
     this.enggList = [
-      {key:1, name: "Machanical" },
-      {key:2, name: "Electronic" },
-      {key:3, name: "Designing" }
+      { key: 1, name: "Machanical" },
+      { key: 2, name: "Electronic" },
+      { key: 3, name: "Designing" }
     ]
 
   }
@@ -88,10 +88,10 @@ export class CustRaiseComplaintComponent implements OnInit {
     return this.formGroup.controls;
   }
   onSubmit(values: any) {
-    
+
     this.charts.createComplaint(
-   this.formGroup.value.name, 
-      this.formGroup.value.EngineerType,      
+      this.formGroup.value.name,
+      this.formGroup.value.EngineerType,
       this.formGroup.value.DateOfjoining,
       "c_status",
       "c_assignBy"
@@ -101,68 +101,81 @@ export class CustRaiseComplaintComponent implements OnInit {
         this.spinner.show();
         this.button1();
         this.notificationservice.success("Raise Complaint successfully")
-      } if (result.status === "error") { this.spinner.show();
+      } if (result.status === "error") {
+        this.spinner.show();
         this.notificationservice.error(" Complaint not Raise")
-      }  
+      }
     })
-       
+
     this.spinner.show();
     this.submitted = true;
-    this.showSuccessMsg=false;
-    this.showInvalidMsg=false;
+    this.showSuccessMsg = false;
+    this.showInvalidMsg = false;
     if (this.formGroup.invalid) {
       return;
     }
     this.formGroup.reset();
   }
-  
+
   getItems(ev: any) {
     const val = ev.target.value;
     if (val && val.trim() != '') {
       this.complaint = this.complaint.filter((item) => {
-       if(parseInt(item.c_id) === parseInt(val)){
-        return parseInt(item.c_id) === parseInt(val);
-       }
-     })
+        if (parseInt(item.c_id) === parseInt(val)) {
+          return parseInt(item.c_id) === parseInt(val);
+        }
+      })
     }
-    if(val.length === 0){
-      this.button1()  
+    if (val.length === 0) {
+      this.button1()
     }
   }
-  button1(){
-    
+
+  button1() {
     this.charts.getAllComplaint().subscribe((data: any) => {
-      this.complaint= data.data.filter(el => {
+      this.complaint = data.data.filter((el: any) => {
         if (el.c_assignBy == window.localStorage.getItem('id')) {
-          AppSettings.status.forEach((s_code:any)=>{
-            if(parseInt(el.c_status) === parseInt(s_code.id)){
-              el.c_status = s_code.value;
-              this.statusOpen = data.data.filter((el:any)=>{
-                return  el.c_status === 1
-              })
-              this.countData.openCount=this.statusOpen.length;
-              this.openCount1=this.countData.openCount
-              
-              this.statusClosed=data.data.filter((el:any)=>{
-                return  el.c_status === 2
-              })
-            
-              this.countData.closedCount=this.statusClosed.length;
-              this.closedCount1=this.countData.closedCount
-            
-              this.statusPending=data.data.filter((el:any)=>{
-                return  el.c_status === 3
-              })
-              this.countData.pendingCount=this.statusPending.length;
-              this.pendingCount1=this.countData.pendingCount
-            }
-          })
+          // AppSettings.status.forEach((s_code:any)=>{
+          if (parseInt(el.c_status) === 1) {
+            // el.c_status = s_code.value;
+            this.statusOpen = data.data.filter((el: any) => {
+              if (el.c_assignBy == window.localStorage.getItem('id')) {
+                return el.c_status === 1
+              }
+            })
+            this.countData.openCount = this.statusOpen.length;
+            this.openCount1 = this.countData.openCount;
+          }
+
+          if (parseInt(el.c_status) === 2) {
+            // el.c_status = s_code.value;
+            this.statusClosed = data.data.filter((el: any) => {
+              if (el.c_assignBy == window.localStorage.getItem('id')) {
+                return el.c_status === 2
+              }
+            })
+            this.countData.closedCount = this.statusClosed.length;
+            this.closedCount1 = this.countData.closedCount;
+          }
+
+          if (parseInt(el.c_status) === 3) {
+            // el.c_status = s_code.value;
+            this.statusPending = data.data.filter((el: any) => {
+              if (el.c_assignBy == window.localStorage.getItem('id')) {
+                return el.c_status === 3
+              }
+            })
+            this.countData.pendingCount = this.statusPending.length;
+            this.pendingCount1 = this.countData.pendingCount;
+          }
           return el;
-        };
-      });
-    });
+        }
+        
+      })
+      
+    }
+    )
 
   }
 }
-
 
